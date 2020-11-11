129890bab3ff9c52b87b529b03fea1b679458385
Antoine Maillard <antoine.maillard@epitech.eu>
2020-11-11 10:14:57 +0100

    update UDPClient

--- app/src/client/Class/TCPClient/responseHandling/joinRoom.cpp
+++ app/src/client/Class/TCPClient/responseHandling/joinRoom.cpp
@@ -6,6 +6,7 @@
 */
 
 #include "client/Class/TCPClient/TCPClient.hpp"
+#include "client/Class/UDPClient/UDPClient.hpp"
 
 void TCPClient::handleJoinRoom()
 {
@@ -34,4 +35,11 @@ void TCPClient::handleInfoServer()
         std::cout << "Cannot get Info Server" << std::endl;
     }
     std::cout << "Server info: " << address << ':' << port << std::endl;
+    UDPClient gameClient(ioService, address, std::to_string(port));
+
+    menu->setScene(sceneName::GAME);
+
+    gameClient.getBuffer().writeInt(666);
+    gameClient.send();
+    gameClient.run();
 }
