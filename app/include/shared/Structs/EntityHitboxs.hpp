18ca2764727ad24f670fbaad3eb76d2ed5700812
michael.fabrie <michael.fabrie@epitech.eu>
2020-11-14 15:19:59 +0100

    projectile hitboxes

--- app/include/shared/Structs/EntityHitboxs.hpp
+++ app/include/shared/Structs/EntityHitboxs.hpp
@@ -11,11 +11,16 @@
 #include "shared/Components/Components.hpp"
 
 #define PlayerHitbox Hitbox{33, 17}
+
 #define Enemy1Hitbox Hitbox{22, 24}
 #define Enemy2Hitbox Hitbox{32, 22}
 #define Enemy3Hitbox Hitbox{48, 48}
 #define Enemy4Hitbox Hitbox{32, 32}
-#define Enemy5Hitbox Hitbox{32, 32}
+#define Enemy5Hitbox Hitbox{33, 32}
+#define Enemy6Hitbox Hitbox{64, 48}
 
+#define Projectile1Hitbox Hitbox{33, 12}
+#define Projectile2Hitbox Hitbox{81, 16}
+#define Projectile3Hitbox Hitbox{33, 32}
 
 #endif /* !ENTITYHITBOXS_HPP_ */
